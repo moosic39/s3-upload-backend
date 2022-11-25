2d0a512b4fec39f4407d4bfcd8c5bd720878ad98
Mickael JEGAT <moosic39@gmail.com>
2022-11-25 11:26:28 +0100

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /*node express app tat uploads and downloads objects from amazon S3*/
 var express = require('express');
-const cors = require( 'cors');
+const cors = require('cors');
 var bodyParser = require('body-parser');
 var fileupload = require("express-fileupload");
 var amazonS3 = require('aws-sdk');
-const { Redshift } = require('aws-sdk');
+require("dotenv").config();
 
 var app = express();
 app.use(cors());
@@ -12,12 +12,12 @@ app.use(fileupload());
 
 // Initializing the amazonS3 object.
 amazonS3.config.update({
-  accessKeyId: "YOUR_ACCESS_KEY_ID",
-  secretAccessKey: "YOUR_SECRET",
-  subregion: "YOUR_REGION"
+    accessKeyId: process.env.YOUR_ACCESS_KEY_ID,
+    secretAccessKey: process.env.YOUR_SECRET,
+    subregion: process.env.YOUR_REGION
 });
 
-var s3Bucket = new amazonS3.S3({ params: { Bucket: "YOUR_BUCKET_NAME" } });
+var s3Bucket = new amazonS3.S3({ params: { Bucket: "mickael-all-together" } });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,9 +28,10 @@ app.post('/api/upload', function (req, res) {
     if (req.files) {
         var file = req.files.file,
             filename = file.name,
-            username=req.body.username;
+            username = req.body.username,
+            timestamp = Date.now()
         var params = {
-            Key: `${username}/${filename}`,
+            Key: `${username}/${timestamp}_${filename}`,
             Body: file.data,
             ContentType: file.mimetype,
             ACL: 'public-read'
